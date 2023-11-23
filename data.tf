86dae0e69773fabc3e3c9b60351cf0f1a2cab488
mcchandu <43166150+mcchandu@users.noreply.github.com>
2023-11-23 12:18:07 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -16,7 +16,3 @@ data "aws_caller_identity" "current" {}
 data "aws_iam_session_context" "current" {
   arn = data.aws_caller_identity.current.arn
 }
-
-data "aws_ssm_parameter" "opsgenie_key" {
-  name = "/${var.environment}/alerts/cloudwatch/opsgenie/key"
-}
